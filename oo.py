894f1e7c094246abd149dfaa4d65c8691f9af142
zeusalgo <email@example.com>
2021-11-13 15:21:53 -0600

    m

---
+++ oo.py
@@ -0,0 +1,7 @@
+import praw as p
+
+rd = p.Reddit(user_agent = '',
+              client_id = '',
+              client_secret = 'DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
+              username = 'PlaceTimely532',
+              password = 'zxh4NG4vAd7PDcp')
