e36928afea92a5b5dacf23f89373b59d4b483828
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2021-11-13 19:57:25 -0600

    m

--- oo.py
+++ oo.py
@@ -1,7 +1,8 @@
 import praw as p
+
 map = {'clientId': '9EMA9bZJNOr-3jDNFPR8Ug', 'clientSecret': 'DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
-           'userAgent': 'web:placetimely532:1(by u/PlaceTimely532)',
-           'username': 'PlaceTimely532', 'password': 'zxh4NG4vAd7PDcp'}
+       'userAgent': 'web:placetimely532:1(by u/PlaceTimely532)',
+       'username': 'PlaceTimely532', 'password': 'zxh4NG4vAd7PDcp'}
 
 rd = p.Reddit(client_id=map['clientId'], client_secret=map['clientSecret'],
               user_agent=map['userAgent'], username=map['username'], password=map['password'])
@@ -11,8 +12,13 @@ readOnly = p.Reddit(client_id=map['clientId'], client_secret=map['clientSecret']
 for idx, post in enumerate(readOnly.subreddit('boxing').hot(limit=5), 1):
     print(idx, post.title)
 
+print(rd.read_only)
+rd.read_only = True
+print(rd.read_only)
+
+for post in rd.subreddit('boxing').hot(limit=3):
+    print(post.title)
+
 title = 'Wilder is amazing'
 body = 'Wilder is not the best boxer. Yet, he remains an remarkable individual given his upbringing.'
 # rd.subreddit('boxing').submit(title=title, selftext=body)
-
-
