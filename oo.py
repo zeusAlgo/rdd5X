5cf761c28dad99833ed36100e020f8a91e583628
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2021-11-13 18:27:30 -0600

    m

--- oo.py
+++ oo.py
@@ -1,7 +1,7 @@
 import praw as p
 
-rd = p.Reddit(user_agent = '',
-              client_id = '',
-              client_secret = 'DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
-              username = 'PlaceTimely532',
-              password = 'zxh4NG4vAd7PDcp')
+rd = p.Reddit(user_agent='',
+              client_id='9EMA9bZJNOr-3jDNFPR8Ug',
+              client_secret='DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
+              username='PlaceTimely532',
+              password='zxh4NG4vAd7PDcp')
