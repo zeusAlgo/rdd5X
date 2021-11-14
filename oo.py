b3b83f837a374291b8a85c0cfe853749dceceb10
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2021-11-13 19:06:54 -0600

    m

--- oo.py
+++ oo.py
@@ -1,11 +1,16 @@
 import praw as p
 
-rd3 = p.Reddit(user_agent='web:placetimely532:1(by u/PlaceTimely532)', client_id='9EMA9bZJNOr-3jDNFPR8Ug',
-               client_secret='DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w', username='PlaceTimely532', password='zxh4NG4vAd7PDcp')
+hashmap = {'clientId':'9EMA9bZJNOr-3jDNFPR8Ug', 'clientSecret':'DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
+              'userAgent':'web:placetimely532:1(by u/PlaceTimely532)',
+              'username':'PlaceTimely532', 'password':'zxh4NG4vAd7PDcp'}
 
 rd = p.Reddit(client_id='9EMA9bZJNOr-3jDNFPR8Ug', client_secret='DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
               user_agent='web:placetimely532:1(by u/PlaceTimely532)',
               username='PlaceTimely532', password='zxh4NG4vAd7PDcp')
+
+readOnly = p.Reddit(client_id=hashmap['clientId'], client_secret=hashmap['clientSecret'], user_agent=hashmap['userAgent'])
+for post in readOnly.subreddit('boxing').hot(limit=3):
+    print(post.title)
 
 title = 'Wilder is amazing'
 body = 'Wilder is not the best boxer. Yet, he remains an remarkable individual given his upbringing.'
@@ -13,5 +18,5 @@ body = 'Wilder is not the best boxer. Yet, he remains an remarkable individual g
 
 print(rd.read_only)
 
-for post in rd.subreddit('boxing').hot(limit=3):
-    print(post.title)
+# for post in rd.subreddit('boxing').hot(limit=3):
+#     print(post.title)
