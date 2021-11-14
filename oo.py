de763b3f8ce366ae6524e75592bb6e0b0599e463
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2021-11-13 18:59:07 -0600

    m

--- oo.py
+++ oo.py
@@ -3,8 +3,9 @@ import praw as p
 rd3 = p.Reddit(user_agent='web:placetimely532:1(by u/PlaceTimely532)', client_id='9EMA9bZJNOr-3jDNFPR8Ug',
                client_secret='DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w', username='PlaceTimely532', password='zxh4NG4vAd7PDcp')
 
-rd = p.Reddit(user_agent='web:placetimely532:1(by u/PlaceTimely532)', client_id='9EMA9bZJNOr-3jDNFPR8Ug',
-              client_secret='DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w', username='PlaceTimely532', password='zxh4NG4vAd7PDcp')
+rd = p.Reddit(client_id='9EMA9bZJNOr-3jDNFPR8Ug', client_secret='DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
+              user_agent='web:placetimely532:1(by u/PlaceTimely532)',
+              username='PlaceTimely532', password='zxh4NG4vAd7PDcp')
 
 title = 'Wilder is amazing'
 body = 'Wilder is not the best boxer. Yet, he remains an remarkable individual given his upbringing.'
