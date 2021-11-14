db1a5eaed4d5c67856166df888ac210efc7c6687
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2021-11-13 19:34:41 -0600

    m

--- oo.py
+++ oo.py
@@ -1,14 +1,12 @@
 import praw as p
-hashmap = {'clientId': '9EMA9bZJNOr-3jDNFPR8Ug', 'clientSecret': 'DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
+map = {'clientId': '9EMA9bZJNOr-3jDNFPR8Ug', 'clientSecret': 'DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
            'userAgent': 'web:placetimely532:1(by u/PlaceTimely532)',
            'username': 'PlaceTimely532', 'password': 'zxh4NG4vAd7PDcp'}
 
-rd = p.Reddit(client_id=hashmap['clientId'], client_secret=hashmap['clientSecret'],
-              user_agent=hashmap['userAgent'],
-              username=hashmap['username'], password=hashmap['password'])
+rd = p.Reddit(client_id=map['clientId'], client_secret=map['clientSecret'],
+              user_agent=map['userAgent'], username=map['username'], password=map['password'])
 
-readOnly = p.Reddit(client_id=hashmap['clientId'], client_secret=hashmap['clientSecret'],
-                    user_agent=hashmap['userAgent'])
+readOnly = p.Reddit(client_id=map['clientId'], client_secret=map['clientSecret'], user_agent=map['userAgent'])
 
 for idx, post in enumerate(readOnly.subreddit('boxing').hot(limit=5), 1):
     print(idx, post.title)
