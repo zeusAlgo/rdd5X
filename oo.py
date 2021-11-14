70f9d7f25f3865be1c9c8fdd84b23adb8149a069
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2021-11-13 19:13:35 -0600

    m

--- oo.py
+++ oo.py
@@ -1,22 +1,25 @@
 import praw as p
 
-hashmap = {'clientId':'9EMA9bZJNOr-3jDNFPR8Ug', 'clientSecret':'DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
-              'userAgent':'web:placetimely532:1(by u/PlaceTimely532)',
-              'username':'PlaceTimely532', 'password':'zxh4NG4vAd7PDcp'}
+hashmap = {'clientId': '9EMA9bZJNOr-3jDNFPR8Ug', 'clientSecret': 'DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
+           'userAgent': 'web:placetimely532:1(by u/PlaceTimely532)',
+           'username': 'PlaceTimely532', 'password': 'zxh4NG4vAd7PDcp'}
 
-rd = p.Reddit(client_id='9EMA9bZJNOr-3jDNFPR8Ug', client_secret='DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
-              user_agent='web:placetimely532:1(by u/PlaceTimely532)',
-              username='PlaceTimely532', password='zxh4NG4vAd7PDcp')
+rd = p.Reddit(client_id=hashmap['clientId'], client_secret=hashmap['clientSecret'],
+              user_agent=hashmap['userAgent'],
+              username=hashmap['username'], password=hashmap['password'])
 
-readOnly = p.Reddit(client_id=hashmap['clientId'], client_secret=hashmap['clientSecret'], user_agent=hashmap['userAgent'])
-for post in readOnly.subreddit('boxing').hot(limit=3):
+readOnly = p.Reddit(client_id=hashmap['clientId'], client_secret=hashmap['clientSecret'],
+                    user_agent=hashmap['userAgent'])
+for post in readOnly.subreddit('boxing').hot(limit=5):
     print(post.title)
+
+for idx, post in readONly.subreddit('')
 
 title = 'Wilder is amazing'
 body = 'Wilder is not the best boxer. Yet, he remains an remarkable individual given his upbringing.'
 # rd.subreddit('boxing').submit(title=title, selftext=body)
 
-print(rd.read_only)
+# print(rd.read_only)
 
 # for post in rd.subreddit('boxing').hot(limit=3):
 #     print(post.title)
