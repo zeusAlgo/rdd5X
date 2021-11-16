93d1b410653bbc7ab858a4f107b9eaad9e1d903c
zeusalgo <email@example.com>
2021-11-15 20:02:49 -0600

    m

--- oo.py
+++ oo.py
@@ -4,7 +4,7 @@ map = {'clientId': '9EMA9bZJNOr-3jDNFPR8Ug', 'clientSecret': 'DjcpA3XYXiO3O0eH0s
        'userAgent': 'web:placetimely532:1(by u/PlaceTimely532)',
        'username': 'PlaceTimely532', 'password': 'zZEKHwSTzDgCCq9'}
 rd, readOnly = p.Reddit(client_id=map['clientId'], client_secret=map['clientSecret'],
-              user_agent=map['userAgent'], username=map['username'], password=map['password']), \
+                        user_agent=map['userAgent'], username=map['username'], password=map['password']), \
                p.Reddit(client_id=map['clientId'], client_secret=map['clientSecret'], user_agent=map['userAgent'])
 
 sub = rd.subreddit('boxing')
@@ -15,5 +15,9 @@ for idx, post in enumerate(readOnly.subreddit('boxing').hot(limit=5), 1):
 for comment in sub.comments(limit=10):
     print(comment.body)
 
-for post in rd.subreddit('boxing').stream.submissions():
-    post.reply('Intriguing perspective')
+
+def comment():
+    for post in rd.subreddit('boxing').stream.submissions():
+        post.reply('Intriguing perspective')
+
+# comment()
