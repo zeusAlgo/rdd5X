03ac677d346c7f198589d471ad621c6c6e5a093b
zeusalgo <email@example.com>
2021-11-15 20:01:07 -0600

    m

--- oo.py
+++ oo.py
@@ -3,12 +3,11 @@ import praw as p
 map = {'clientId': '9EMA9bZJNOr-3jDNFPR8Ug', 'clientSecret': 'DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
        'userAgent': 'web:placetimely532:1(by u/PlaceTimely532)',
        'username': 'PlaceTimely532', 'password': 'zZEKHwSTzDgCCq9'}
+rd, readOnly = p.Reddit(client_id=map['clientId'], client_secret=map['clientSecret'],
+              user_agent=map['userAgent'], username=map['username'], password=map['password']), \
+               p.Reddit(client_id=map['clientId'], client_secret=map['clientSecret'], user_agent=map['userAgent'])
 
-rd = p.Reddit(client_id=map['clientId'], client_secret=map['clientSecret'],
-              user_agent=map['userAgent'], username=map['username'], password=map['password'])
 sub = rd.subreddit('boxing')
-
-readOnly = p.Reddit(client_id=map['clientId'], client_secret=map['clientSecret'], user_agent=map['userAgent'])
 
 for idx, post in enumerate(readOnly.subreddit('boxing').hot(limit=5), 1):
     print(idx, post.title)
