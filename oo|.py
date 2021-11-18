2c1dc287efd7522768cd68c5ff551fda9f82647e
zeusalgo <email@example.com>
2021-11-18 17:34:23 -0600

    m

---
+++ oo|.py
@@ -0,0 +1,65 @@
+import praw as p
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPTJForCausalLM
+from transformers import GPTNeoForCausalLM
+import torch.nn
+from transformers import pipeline
+import yaml
+import torch.distributed
+
+map = {'clientId': '9EMA9bZJNOr-3jDNFPR8Ug', 'clientSecret': 'DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
+       'userAgent': 'web:placetimely532:1(by u/PlaceTimely532)',
+       'username': 'PlaceTimely532', 'password': 'zZEKHwSTzDgCCq9'}
+rd, readOnly = p.Reddit(client_id=map['clientId'], client_secret=map['clientSecret'],
+                        user_agent=map['userAgent'], username=map['username'], password=map['password']), \
+               p.Reddit(client_id=map['clientId'], client_secret=map['clientSecret'], user_agent=map['userAgent'])
+sub = rd.subreddit('gaming')
+
+
+def commEnum(reqComm):
+    for idx, comment in enumerate(sub.comments(limit=10), 1):
+        print(idx, comment.body)
+
+
+def postEnum(reqPosts):
+    for idx, post in enumerate(sub.hot(limit=reqPosts), 1):
+        print(idx, post.title)
+        if idx == 5: print('--')
+
+
+def gen(prompt):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f'Using {torch.cuda.get_device_name()}')
+    tokenizer, model = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B"), \
+                       GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-2.7B").to(device)
+    inputIds = tokenizer.encode(prompt, return_tensors='pt').to(device)
+    greedy_output = model.generate(inputIds, temperature=10, max_length=100).to(device)
+    print("Output:\n" + 100 * '-')
+    print(tokenizer.decode(greedy_output[0], skip_special_tokens=True))
+
+
+for idx, post in enumerate(sub.stream.submissions()):
+    if idx == 0:
+        print(post.title)
+        gen(post.title)
+    else:
+        break
+
+
+def comment():
+    for idx, post in enumerate(sub.stream.submissions()):
+        if idx == 0:
+            gen(post)
+            string = post
+            post.reply('Antón Castillo will feel like a real and credible threat. He is more of a satirical mash-up '
+                       'of Franco and Castro. He is certainly a bad guy with a vision for his country that he holds '
+                       'above all—even family, as he often reminds his son and protégé, Diego—but there is a '
+                       'two-dimensional vibe I just can not shake.')
+        else:
+            break
+
+
+postEnum(5)
+commEnum(5)
+# comment()
