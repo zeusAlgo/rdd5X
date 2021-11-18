e0a942b5f0a745c19a3268303b73be876dfc604a
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2021-11-17 22:58:22 -0600

    m

--- oo.py
+++ oo.py
@@ -23,25 +23,10 @@ def postEnum(reqPosts):
         if idx == 5: print('--')
 
 
-def gen():
-
+def gen(prompt):
     model = AutoModelForCausalLM.from_pretrained("gpt2-xl")
     tokenizer = AutoTokenizer.from_pretrained("gpt2-xl")
-
-    # Padding text helps XLNet with short prompts - proposed by Aman Rusia in https://github.com/rusiaaman/XLNet-gen#methodology
-    PADDING_TEXT = """In 1991, the remains of Russian Tsar Nicholas II and his family
-    (except for Alexei and Maria) are discovered.
-    The voice of Nicholas's young son, Tsarevich Alexei Nikolaevich, narrates the
-    remainder of the story. 1883 Western Siberia,
-    a young Grigori Rasputin is asked by his father and a group of men to perform magic.
-    Rasputin has a vision and denounces one of the men as a horse thief. Although his
-    father initially slaps him for making such an accusation, Rasputin watches as the
-    man is chased outside and beaten. Twenty years later, Rasputin sees a vision of
-    the Virgin Mary, prompting him to become a priest. Rasputin quickly becomes famous,
-    with people, even a bishop, begging for his blessing. <eod> </s> <eos>"""
-
-    prompt = "Today the weather is really nice and I am planning on "
-    inputs = tokenizer(PADDING_TEXT + prompt, add_special_tokens=False, return_tensors="pt")["input_ids"]
+    inputs = tokenizer(prompt, add_special_tokens=False, return_tensors="pt")["input_ids"]
 
     prompt_length = len(tokenizer.decode(inputs[0]))
     outputs = model.generate(inputs, max_length=250, do_sample=True, top_p=0.95, top_k=60)
@@ -49,10 +34,18 @@ def gen():
     print(generated)
 
 
+for idx, post in enumerate(sub.stream.submissions()):
+    if idx == 0:
+        gen(post)
+    else:
+        break
+
+
 def comment():
     for idx, post in enumerate(sub.stream.submissions()):
         if idx == 0:
-
+            gen(post)
+            string = post
             post.reply('Antón Castillo will feel like a real and credible threat. He is more of a satirical mash-up '
                        'of Franco and Castro. He is certainly a bad guy with a vision for his country that he holds '
                        'above all—even family, as he often reminds his son and protégé, Diego—but there is a '
@@ -63,5 +56,4 @@ def comment():
 
 postEnum(5)
 commEnum(5)
-gen()
 # comment()
