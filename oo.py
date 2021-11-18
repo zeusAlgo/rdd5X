c6169ec48997749c2d0e7427a7e88ea816ece561
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2021-11-17 22:46:04 -0600

    m

--- oo.py
+++ oo.py
@@ -1,6 +1,7 @@
 import praw as p
 import transformers as t
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 map = {'clientId': '9EMA9bZJNOr-3jDNFPR8Ug', 'clientSecret': 'DjcpA3XYXiO3O0eH0sEwrrF_xkzx8w',
        'userAgent': 'web:placetimely532:1(by u/PlaceTimely532)',
@@ -24,14 +25,28 @@ def postEnum(reqPosts):
 
 def gen():
 
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2-xl')
-    model = GPT2LMHeadModel.from_pretrained('gpt2-xl')
+    model = AutoModelForCausalLM.from_pretrained("gpt2-xl")
+    tokenizer = AutoTokenizer.from_pretrained("gpt2-xl")
 
-    inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
-    outputs = model(**inputs, labels=inputs["input_ids"])
-    loss = outputs.loss
-    logits = outputs.logits
-    print(outputs)
+    # Padding text helps XLNet with short prompts - proposed by Aman Rusia in https://github.com/rusiaaman/XLNet-gen#methodology
+    PADDING_TEXT = """In 1991, the remains of Russian Tsar Nicholas II and his family
+    (except for Alexei and Maria) are discovered.
+    The voice of Nicholas's young son, Tsarevich Alexei Nikolaevich, narrates the
+    remainder of the story. 1883 Western Siberia,
+    a young Grigori Rasputin is asked by his father and a group of men to perform magic.
+    Rasputin has a vision and denounces one of the men as a horse thief. Although his
+    father initially slaps him for making such an accusation, Rasputin watches as the
+    man is chased outside and beaten. Twenty years later, Rasputin sees a vision of
+    the Virgin Mary, prompting him to become a priest. Rasputin quickly becomes famous,
+    with people, even a bishop, begging for his blessing. <eod> </s> <eos>"""
+
+    prompt = "Today the weather is really nice and I am planning on "
+    inputs = tokenizer(PADDING_TEXT + prompt, add_special_tokens=False, return_tensors="pt")["input_ids"]
+
+    prompt_length = len(tokenizer.decode(inputs[0]))
+    outputs = model.generate(inputs, max_length=250, do_sample=True, top_p=0.95, top_k=60)
+    generated = prompt + tokenizer.decode(outputs[0])[prompt_length + 1:]
+    print(generated)
 
 
 def comment():
@@ -42,7 +57,8 @@ def comment():
                        'of Franco and Castro. He is certainly a bad guy with a vision for his country that he holds '
                        'above all—even family, as he often reminds his son and protégé, Diego—but there is a '
                        'two-dimensional vibe I just can not shake.')
-        else: break
+        else:
+            break
 
 
 postEnum(5)
